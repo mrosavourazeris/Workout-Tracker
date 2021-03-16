ec1333302dc67453297a90bc685568c441721f93
Michael Rosa-Vourazeris <mrosavourazeris@gmail.com>
2021-03-16 18:26:26 -0500

    Finished post route on api-routes.js. Started on put route.

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,14 +1,23 @@
 const router = require("express").Router();
 const db = require("../models")
 
-router.post("/api/workouts", ({ body }, res) => {
-    console.log(body)
-    // db.Workout.create(body)
-    //     .then(dbWorkout => {
-    //         res.json(dbWorkout)
-    //     })
-    //     .catch(err => {
-    //         res.status(400).json(err)
-    //     })
+router.post("/api/workouts", (req, res) => {
+    console.log(req.body)
+    db.Workout.create(req.body)
+        .then(dbWorkout => {
+            res.json(dbWorkout)
+        })
+        .catch(err => {
+            res.status(400).json(err)
+        })
 })
 
+router.put("/api/workouts/:id", (req,res) => {
+    console.log(req.body)
+    const body = req.body
+    console.log(req.params.id)
+    const id = req.params.id
+    // db.Workout.findByIdAndUpdate(id, {$push: {exercises: body }}, {new: true}, cb)
+})
+
+module.exports = router
